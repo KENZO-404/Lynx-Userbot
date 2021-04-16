c4169367b58ad5890a64fd670b0ae56f9af52cec
Axel <80487896+KENZO-404@users.noreply.github.com>
2021-04-16 17:42:02 +0000

    Update updater.py

--- userbot/modules/updater.py
+++ userbot/modules/updater.py
@@ -17,7 +17,8 @@ from userbot import (
     HEROKU_API_KEY,
     HEROKU_APP_NAME,
     UPSTREAM_REPO_URL,
-    UPSTREAM_REPO_BRANCH)
+    UPSTREAM_REPO_BRANCH,
+    UPSTREAM_REMOTE)
 from userbot.events import register
 
 requirements_path = path.join(
